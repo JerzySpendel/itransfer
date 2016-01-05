f7c7e7340b8ac210f9c87bc3e7001197575c8152
Jerzy Spendel <jspendel@gmail.com>
2016-01-05 14:20:37 +0000

    If downloading, ip address should be specified

--- main.py
+++ main.py
@@ -18,7 +18,7 @@ port = 8888 if not args.port else args.port
 if args.serve:
     print('ok, tu')
     path = args.serve
-    coro = loop.create_server(lambda *args, **kwargs: ServerProtocol(path, *args, **kwargs), '127.0.0.1', port)
+    coro = loop.create_server(lambda *args, **kwargs: ServerProtocol(path, *args, **kwargs), '0.0.0.0', port)
     server = loop.run_until_complete(coro)
     try:
         loop.run_forever()
@@ -30,7 +30,7 @@ if args.serve:
 elif args.download_to or args.download_as:
     print('Downloading...')
     loop = asyncio.get_event_loop()
-    coro = loop.create_connection(lambda: ClientProtocol(args), '127.0.0.1', port)
+    coro = loop.create_connection(lambda: ClientProtocol(args), args.ip, port)
     loop.run_until_complete(coro)
     loop.run_forever()
     loop.close()
--- utils.py
+++ utils.py
@@ -7,3 +7,5 @@ def cli_valid(parsed, parser):
         parser.error("You cannot specify file to serve, and in the same time directory to download")
     elif parsed.download_to and parsed.download_as:
         parser.error("You cannot specify --download-as and download-to in the same time")
+    elif (parsed.download_to or parsed.download_as) and not parsed.ip:
+        parser.error("Well, I would like to download that file for you but where should I search for it? Specify --ip")
